3c7ec89fc84dc5bb3b5d78ac211c04f068a2b7bc
Mwangi-Jack <jasonmuturi2000@gmail.com>
2024-02-08 13:32:26 +0300

    corrections on the BaseModel class

--- models/base_model.py
+++ models/base_model.py
@@ -1,39 +1,64 @@
 #!/usr/bin/python3
 
-"""This is a base model class it defines all common attributes/methods for  other classes"""
+"""This module defines the BaseModel class, which serves as the base model for other classes."""
 
 import uuid
 from datetime import datetime
-
 
 class BaseModel:
     """BaseModel Class"""
 
     def __init__(self, *args, **kwargs):
+        """
+        Initializes a new instance of the BaseModel class.
+
+        Args:
+            *args: Variable length argument list.
+            **kwargs: Arbitrary keyword arguments.
+                id (str): Unique identifier for the instance.
+                created_at (str): ISO formatted string representing the creation date and time.
+
+        If kwargs is provided, the instance attributes are initialized using the provided values,
+        otherwise, new values are generated for id and created_at attributes.
+
+        Returns:
+            None
+        """
         if kwargs:
             self.id = kwargs["id"]
             self.created_at = kwargs["created_at"]
-            # self.id = str(uuid.uuid4())
-            # self.created_at = datetime.now().isoformat()
         else:
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
+            self.created_at = datetime.now().isoformat()
 
         self.__dict__["__class__"] = type(self).__name__
         self.updated_at = datetime.now().isoformat()
 
-
     def save(self):
-        """updates the updated_at attribute with the current time"""
+        """
+        Updates the updated_at attribute with the current date and time.
+
+        Returns:
+            str: Updated value of updated_at attribute (ISO formatted string).
+        """
         self.updated_at = datetime.now().isoformat()
         return self.updated_at
 
     def to_dict(self):
-        """returns a dictionary containing all keys/values of __dict__ of the instance"""
+        """
+        Returns a dictionary containing all keys/values of __dict__ of the instance.
+
+        Returns:
+            dict: Dictionary containing instance attributes.
+        """
         return self.__dict__
 
-
     def __str__(self):
-        """This Returns a string representation of an instance"""
+        """
+        Returns a string representation of the instance.
+
+        Returns:
+            str: String representation of the instance.
+        """
         class_name = type(self).__name__
         return f"[{class_name}] ({self.id}) {self.__dict__}]"
