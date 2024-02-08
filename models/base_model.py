73574c1230e956990dda0202b8f71433545dbb25
Mwangi-Jack <jasonmuturi2000@gmail.com>
2024-02-08 13:21:35 +0300

    corrections on the BaseModel class

--- models/base_model.py
+++ models/base_model.py
@@ -9,19 +9,27 @@ from datetime import datetime
 class BaseModel:
     """BaseModel Class"""
 
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now().isoformat()
-        self.updated_at = datetime.now().isoformat()
+    def __init__(self, *args, **kwargs):
+        if kwargs:
+            self.id = kwargs["id"]
+            self.created_at = kwargs["created_at"]
+            # self.id = str(uuid.uuid4())
+            # self.created_at = datetime.now().isoformat()
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+
         self.__dict__["__class__"] = type(self).__name__
+        self.updated_at = datetime.now().isoformat()
+
 
     def save(self):
         """updates the updated_at attribute with the current time"""
         self.updated_at = datetime.now().isoformat()
+        return self.updated_at
 
     def to_dict(self):
         """returns a dictionary containing all keys/values of __dict__ of the instance"""
-        # class_object = self.__dict__[__class__] = type(self).__name__
         return self.__dict__
 
 
