6fd8620d70e44d5611de9f9efe561edab64442d4
Mwangi-Jack <jasonmuturi2000@gmail.com>
2024-02-08 13:34:32 +0300

    corrections on the BaseModel class

--- models/base_model.py
+++ models/base_model.py
@@ -29,7 +29,7 @@ class BaseModel:
             self.created_at = kwargs["created_at"]
         else:
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.now().isoformat()
+            self.created_at = datetime.now()
 
         self.__dict__["__class__"] = type(self).__name__
         self.updated_at = datetime.now().isoformat()
