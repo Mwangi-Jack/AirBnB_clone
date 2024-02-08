1337dfaa1bdf80fd73241233c4fe0d85490585ed
Mwangi-Jack <jasonmuturi2000@gmail.com>
2024-02-08 18:13:03 +0300

    added file storage engine

--- models/base_model.py
+++ models/base_model.py
@@ -4,6 +4,7 @@
 
 import uuid
 from datetime import datetime
+from models import storage
 
 class BaseModel:
     """BaseModel Class"""
@@ -24,6 +25,10 @@ class BaseModel:
         Returns:
             None
         """
+
+        if not kwargs["id"]:
+            storage.new(kwargs)
+
         if kwargs:
             self.id = kwargs["id"]
             self.created_at = kwargs["created_at"]
@@ -31,8 +36,11 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
 
+
+
         self.__dict__["__class__"] = type(self).__name__
         self.updated_at = datetime.now().isoformat()
+
 
     def save(self):
         """
@@ -42,7 +50,8 @@ class BaseModel:
             str: Updated value of updated_at attribute (ISO formatted string).
         """
         self.updated_at = datetime.now().isoformat()
-        return self.updated_at
+        storage.save()
+        # return self.updated_at
 
     def to_dict(self):
         """
