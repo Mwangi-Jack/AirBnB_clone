e58baa9f7c1ad0c090a306d7f6350cbf8084e800
Mwangi-Jack <jasonmuturi2000@gmail.com>
2024-02-11 18:27:19 +0300

    code to pass pycodestyle

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -3,27 +3,26 @@
 import json
 import os
 
-"""This class simply serializes instaces to a JSON file and deserializes JSON file to instnaces"""
+"""
+This class simply serializes instaces to a JSON file and deserializes
+JSON file to instnaces
+"""
+
 
 class FileStorage:
     """FileStorage class"""
     __file_path = 'file.json'
     __objects = {}
 
-
-
     def all(self):
-        """returns  a dictionary of all the objects in the __objects attribute"""
+        """
+        returns  a dictionary of all the objects in the __objects attribute
+        """
         return self.__objects
 
-    def new(self,obj):
-        """sets in __objects the obj with the key <obj class name>.id
-
-        Args:
-			obj: Object to be added to __objects
-
-		Returns:
-			None
+    def new(self, obj):
+        """
+        sets in __objects the obj with the key <obj class name>.id
         """
         class_name = obj.__class__.__name__
         obj_id = obj.id
@@ -49,7 +48,7 @@ class FileStorage:
                 data = json.load(json_file)
                 for key, value in data.items():
                     class_name, obj_id = key.split('.')
-                    module = __import__('models.base_model', fromlist=[class_name])
+                    module = __import__('models.base_model',
+                                        fromlist=[class_name])
                     cls = getattr(module, class_name)
                     self.__objects[key] = cls(**value)
-
