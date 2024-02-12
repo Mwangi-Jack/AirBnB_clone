0ce6faf07eaa1724feb45ffd044223772de471ff
Mwangi-Jack <jasonmuturi2000@gmail.com>
2024-02-12 11:21:44 +0300

    code to pass pycodestyle

--- tests/test_base_model.py
+++ tests/test_base_model.py
@@ -2,7 +2,6 @@
 
 import unittest
 from datetime import datetime
-import uuid
 from ..models.base_model import BaseModel
 
 
@@ -11,34 +10,35 @@ class TestBaseModel(unittest.TestCase):
 
     def setUp(self):
         """Set up a BaseModel instance for testing."""
-        self.base_model = BaseModel()
+        self.new_model = BaseModel()
 
     def test_constructor(self):
         """Test the constructor of the BaseModel class."""
-        self.assertIsInstance(self.base_model.id, str)
-        self.assertIsInstance(self.base_model.created_at, datetime)
-        self.assertIsInstance(self.base_model.updated_at, datetime)
+        self.assertIsInstance(self.new_model.id, str)
+        self.assertIsInstance(self.new_model.created_at, datetime)
+        self.assertIsInstance(self.new_model.updated_at, datetime)
 
     def test_save(self):
         """Test the save method of the BaseModel class."""
-        previous_updated_at = self.base_model.updated_at
-        self.base_model.save()
-        self.assertNotEqual(previous_updated_at, self.base_model.updated_at)
+        previous_updated_at = self.new_model.updated_at
+        self.new_model.save()
+        self.assertNotEqual(previous_updated_at, self.new_model.updated_at)
 
     def test_to_dict(self):
         """Test the to_dict method of the BaseModel class."""
         expected_dict = {
-            'id': self.base_model.id,
-            'created_at': self.base_model.created_at.isoformat(),
-            'updated_at': self.base_model.updated_at.isoformat(),
+            'id': self.new_model.id,
+            'created_at': self.new_model.created_at.isoformat(),
+            'updated_at': self.new_model.updated_at.isoformat(),
             '__class__': 'BaseModel'
         }
-        self.assertDictEqual(expected_dict, self.base_model.to_dict())
+        self.assertDictEqual(expected_dict, self.new_model.to_dict())
 
     def test_str(self):
         """Test the string representation of the BaseModel class."""
-        expected_string = f"[BaseModel] ({self.base_model.id}) {self.base_model.__dict__}"
-        self.assertEqual(expected_string, str(self.base_model))
+        _str = f"[BaseModel] ({self.new_model.id}) {self.new_model.__dict__}"
+        self.assertEqual(_str, str(self.new_model))
+
 
 if __name__ == '__main__':
     unittest.main()
