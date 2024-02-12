52c17f059982deb76c57753d2c4b97193b154c66
Mwangi-Jack <jasonmuturi2000@gmail.com>
2024-02-12 17:12:06 +0300

    created basic commands for the console

--- console.py
+++ console.py
@@ -8,11 +8,11 @@ class HBNBCommand(cmd.Cmd):
 
     prompt = "(hbnb) "
 
-    def do_EOF(self, args):
-        """Command to exit the program"""
+    def do_EOF(self, arg):
+        """EOF command to exit the program"""
         return True
 
-    def do_quit(self, args):
+    def do_quit(self, arg):
         """Quit command to exit the program"""
         return True
 
