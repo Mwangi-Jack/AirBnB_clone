039df03f11ed51b82c21e2b8bf4034d19e80b731
Mwangi-Jack <jasonmuturi2000@gmail.com>
2024-02-14 10:39:21 +0300

    wrote tests for User class

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 
 import unittest
-from unittest.mock import patch
 from models.user import User
 from models.base_model import BaseModel
 
@@ -14,40 +13,15 @@ class TestUser(unittest.TestCase):
         self.user = User()
 
     def test_inheritance(self):
-        """Test if User class inherits from BaseModel class."""
+        """Test that User inherits from BaseModel."""
         self.assertIsInstance(self.user, BaseModel)
 
-    def test_attributes(self):
-        """Test if User instance has the expected attributes."""
-        self.assertTrue(hasattr(self.user, 'email'))
-        self.assertTrue(hasattr(self.user, 'password'))
-        self.assertTrue(hasattr(self.user, 'first_name'))
-        self.assertTrue(hasattr(self.user, 'last_name'))
-
-    @patch('models.storage')
-    def test_new(self, mock_storage):
-        """Test if new method correctly adds the User object to storage."""
-        user = User()
-        self.assertEqual(len(mock_storage.new.call_args_list), 1)
-        self.assertEqual(mock_storage.new.call_args[0][0], user)
-
-    @patch('models.storage')
-    def test_init_with_args(self, mock_storage):
-        """Test if User initialization with arguments works correctly."""
-        data = {
-            'email': 'test@example.com',
-            'password': 'password123',
-            'first_name': 'John',
-            'last_name': 'Doe'
-        }
-        user = User(**data)
-        # self.assertEqual(user.id, data['id'])
-        self.assertEqual(user.email, data['email'])
-        self.assertEqual(user.password, data['password'])
-        self.assertEqual(user.first_name, data['first_name'])
-        self.assertEqual(user.last_name, data['last_name'])
-        self.assertEqual(len(mock_storage.new.call_args_list), 1)
-        self.assertEqual(mock_storage.new.call_args[0][0], user)
+    def test_initialization(self):
+        """Test the initialization of the User class."""
+        self.assertEqual(self.user.email, "")
+        self.assertEqual(self.user.password, "")
+        self.assertEqual(self.user.first_name, "")
+        self.assertEqual(self.user.last_name, "")
 
 
 if __name__ == '__main__':
