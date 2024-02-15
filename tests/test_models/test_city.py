1e5f7656773493bd192f7a20564544ecfdaf60db
Mwangi-Jack <jasonmuturi2000@gmail.com>
2024-02-15 13:54:35 +0300

    wrotet tests for all the other classes that inherit from BaseModel

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -1,12 +1,18 @@
 #!/usr/bin/pthon3
 
 import unittest
+import uuid
 from models.city import City
 from models.base_model import BaseModel
 
 
 class TestCity(unittest.TestCase):
     """Test cases for the City class."""
+
+    def setUp(self):
+        self.new_city = City()
+        self.new_city.state_id = str(uuid.uuid4())
+        self.new_city.name = 'Bahamas'
 
     def test_inheritance(self):
         """Test inheritance from BaseModel."""
@@ -20,33 +26,30 @@ class TestCity(unittest.TestCase):
 
     def test_save_method(self):
         """Test the save method of the City class."""
-        city = City()
-        city.save()
-        self.assertIsNotNone(city.created_at)
-        self.assertIsNotNone(city.updated_at)
+        self.new_city.save()
+        self.assertIsNotNone(self.new_city.created_at)
+        self.assertIsNotNone(self.new_city.updated_at)
 
     def test_to_dict_method(self):
         """Test the to_dict method of the City class."""
-        city = City()
-        city_dict = city.to_dict()
+        city_dict = self.new_city.to_dict()
         self.assertEqual(city_dict['__class__'], 'City')
         self.assertIn('id', city_dict)
         self.assertIn('created_at', city_dict)
         self.assertIn('updated_at', city_dict)
-        # self.assertIn('state_id', city_dict)
-        # self.assertIn('name', city_dict)
+        self.assertIn('state_id', city_dict)
+        self.assertIn('name', city_dict)
 
     def test_str_method(self):
         """Test the __str__ method of the City class."""
-        city = City()
-        city_str = str(city)
+        city_str = str(self.new_city)
 
         self.assertIn("[City]", city_str)
         self.assertIn("'id':", city_str)
         self.assertIn("'created_at':", city_str)
         self.assertIn("'updated_at':", city_str)
-        # self.assertIn("'state_id':", city_str)
-        # self.assertIn("'name':", city_str)
+        self.assertIn("'state_id':", city_str)
+        self.assertIn("'name':", city_str)
 
 
 if __name__ == '__main__':
